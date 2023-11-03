b4ca04b0f4873833bf2c19df7caeedf73e6d60c8
Lidia-Gabriela Burca <Lidia.Burca@centric.eu>
2023-11-03 09:27:40 +0200

    Add AnotherStringManager class

---
+++ CSharpPractice/AnotherStringManager.cs
@@ -0,0 +1,105 @@
+﻿namespace CSharpPractice
+{
+    public class AnotherStringManager : IStringManager
+    {
+        public int GetStringLength(string word)
+        {
+            int counter = 0;
+            foreach (char c in word)
+            {
+                counter++;
+            }
+            return counter;
+        }
+
+        public int GetNumberOfLetterAppearances(string word, char letter)
+        {
+            var nrOfAppearances = word.Split(letter).Length - 1;
+            return nrOfAppearances;
+        }
+
+        public int GetNumberOfVowels(string word)
+        {
+            var counter = 0;
+            var vowels = new[] { 'a', 'e', 'i', 'o', 'u' };
+            foreach (var vowel in vowels)
+            {
+                counter += GetNumberOfLetterAppearances(word, vowel);
+            }
+            return counter;
+        }
+
+        public int GetNumberOfWords(string phrase)
+        {
+            phrase = phrase.Trim();
+            var counter = 0;
+            if (phrase.Length > 0)
+            {
+                counter = 1;
+                foreach (char c in phrase)
+                {
+                    if (c == ' ')
+                    {
+                        counter++;
+                    }
+
+                }
+            }
+
+            return counter;
+        }
+
+        public string RemoveCharacter(string word, char character)
+        {
+            foreach (char c in word)
+            {
+                if (c == character)
+                {
+                    int pos = word.IndexOf(c);
+                    if (pos >= 0)
+                    {
+                        word = word.Remove(pos, 1);
+                    }
+                }
+            }
+            return word;
+        }
+
+        public bool CheckStringsEquality(string firstWord, string secondWord)
+        {
+            if (firstWord.Length != secondWord.Length)
+            {
+                return false;
+            }
+
+            return String.Compare(firstWord, secondWord) == 0;
+        }
+
+        public string GetStringBackwards(string word)
+        {
+            var wordArray = word.ToCharArray();
+            var backword = "";
+            for (int i = wordArray.Length - 1; i >= 0; i--)
+            {
+                backword += wordArray[i];
+            }
+            return backword;
+        }
+
+        public bool IsStringPalindrome(string word)
+        {
+            var wordArray = word.ToCharArray();
+            var wordReverseArray = word.Reverse().ToArray();
+
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                if (wordArray[i] != wordReverseArray[i])
+                    return false;
+            }
+            return true;
+
+            //return wordArray == wordReverseArray;
+        }
+
+    }
+}
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -1,10 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using CSharpPractice;
-
+﻿using CSharpPractice;
 
 var stringManager = new StringManager();
+var anotherStringManager = new AnotherStringManager();
 
+Console.WriteLine("     1. stringManager");
 Console.WriteLine("GetStringLength: " + stringManager.GetStringLength("testword"));
 Console.WriteLine("GetNumberOfLetterAppearances: " + stringManager.GetNumberOfLetterAppearances("testword", 't'));
 Console.WriteLine("GetNumberOfVowels: " + stringManager.GetNumberOfVowels("Today is a good day"));
@@ -13,3 +12,15 @@ Console.WriteLine("RemoveCharacter: " + stringManager.RemoveCharacter("abecedar"
 Console.WriteLine("CheckStringsEquality: " + stringManager.CheckStringsEquality("abecedar", "abecedar"));
 Console.WriteLine("GetStringBackwards: " + stringManager.GetStringBackwards("abecedar"));
 Console.WriteLine("IsStringPalindrome: " + stringManager.IsStringPalindrome("ana"));
+
+Console.WriteLine("\n");
+
+Console.WriteLine("     2. anotherStringManager");
+Console.WriteLine("GetStringLength: " + anotherStringManager.GetStringLength("testword"));
+Console.WriteLine("GetNumberOfLetterAppearances: " + anotherStringManager.GetNumberOfLetterAppearances("testword", 't'));
+Console.WriteLine("GetNumberOfVowels: " + anotherStringManager.GetNumberOfVowels("Today is a good day"));
+Console.WriteLine("GetNumberOfWords: " + anotherStringManager.GetNumberOfWords("Today is a good day"));
+Console.WriteLine("RemoveCharacter: " + anotherStringManager.RemoveCharacter("abecedar", 'a'));
+Console.WriteLine("CheckStringsEquality: " + anotherStringManager.CheckStringsEquality("abecedar", "abecedar"));
+Console.WriteLine("GetStringBackwards: " + anotherStringManager.GetStringBackwards("abecedar"));
+Console.WriteLine("IsStringPalindrome: " + anotherStringManager.IsStringPalindrome("ana"));
